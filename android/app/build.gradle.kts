1577d6fa8955cceab414ce52e658ad43ebe94d81
Alexander Linder <seine@eloque.nz>
2026-06-18 21:28:25 +0200

    Fix signing config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -58,11 +58,13 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            signingConfig = signingConfigs["release"]
         }
     }
     buildFeatures {
