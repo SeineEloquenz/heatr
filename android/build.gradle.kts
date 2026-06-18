5fe5e1ba9f634f4735d844e04d4878a0fdc8814c
Alexander Linder <seine@eloque.nz>
2026-06-18 18:59:15 +0200

    Add ktlint to gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,4 +2,10 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint)
+}
+
+configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+    outputToConsole.set(true)
+    outputColorName.set("RED")
 }
