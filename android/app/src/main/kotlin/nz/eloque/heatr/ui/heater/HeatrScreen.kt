c1f433904f89be5c0d34175144e68976f5441e8a
Alexander Linder <seine@eloque.nz>
2026-06-18 18:11:12 +0200

    Fix imports

--- android/app/src/main/kotlin/nz/eloque/heatr/ui/heater/HeatrScreen.kt
+++ android/app/src/main/kotlin/nz/eloque/heatr/ui/heater/HeatrScreen.kt
@@ -1,6 +1,5 @@
 package nz.eloque.heatr.ui.heater
 
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -18,7 +17,6 @@ import androidx.compose.material.icons.outlined.AccessTime
 import androidx.compose.material.icons.outlined.ChildCare
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Spa
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults.elevatedCardColors
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
@@ -33,16 +31,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import nz.eloque.heatr.R
 import nz.eloque.heatr.api.Duration
 import nz.eloque.heatr.api.Generation
-import nz.eloque.heatr.api.HeatingPhase
 import nz.eloque.heatr.api.SkinSensitivity
-import nz.eloque.heatr.ui.components.PulsingDot
 import nz.eloque.heatr.ui.components.TreatmentActionCard
 
 @Composable
