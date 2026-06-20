c3784edae9d4d5d6a83a0ae2a424f1d999ecaea7
Alexander Linder <seine@eloque.nz>
2026-06-20 17:45:05 +0200

    Update compose-kit dependency config

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven {
-            url = uri("https://jitpack.io")
+            url = uri("https://raw.githubusercontent.com/SeineEloquenz/compose-kit/maven/")
         }
     }
 }
