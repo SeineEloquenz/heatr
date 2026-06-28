a43be0c94eb0e95541789f46ef35d846492e10ce
Alexander Linder <seine@eloque.nz>
2026-06-28 10:42:24 +0200

    Bump version to v0.2.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "nz.eloque.heatr"
         minSdk = 28
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
